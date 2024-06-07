208ecf743fbcb2e333f4ce9f957a9612b4b63dee
Dendha Arthansa <148839330+alrghtden@users.noreply.github.com>
2024-06-07 14:11:58 +0700

    Update klaim_sukses.php

--- klaim_sukses.php
+++ klaim_sukses.php
@@ -16,18 +16,7 @@
         if (isset($_GET['kode_unik'])) {
             $kode_unik = $_GET['kode_unik'];
 
-            include('phpqrcode/qrlib.php');
-
-            $tempDir = 'temp/';
-            if (!file_exists($tempDir)){
-                mkdir($tempDir);
-            }
-            $qrCodeFile = $tempDir . 'qrcode_' . $kode_unik . '.png';
-            QRcode::png($kode_unik, $qrCodeFile, QR_ECLEVEL_L, 10);
-
             echo "<p>Silakan tunjukkan kode unik ini kepada petugas untuk mengambil barang: <br><h1>$kode_unik</h1></p>";
-            echo "<p>Atau tunjukkan QR Code berikut:</p>";
-            echo "<img src='$qrCodeFile' alt='QR Code'>";
         } else {
             echo "<p>Kesalahan: Kode unik tidak ditemukan.</p>";
         }
